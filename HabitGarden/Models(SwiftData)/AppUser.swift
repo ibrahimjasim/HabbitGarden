ed27d5f10407b6e21a2b986f25cf775b2cc63007
Ibrahem Jasim Alsalih <alazawy.m91@gmail.com>
2026-05-07 22:45:11 +0200

    add AppUser model to fix missing type errors in AuthViewModel

---
+++ HabitGarden/Models(SwiftData)/AppUser.swift
@@ -0,0 +1,14 @@
+//
+//  AppUser.swift
+//  HabitGarden
+//
+//  Created by Ibrahim Jasim Alsalih on 2026-05-07.
+//
+
+import Foundation
+
+struct AppUser: Codable {
+    let id: String
+    let name: String
+    let email: String?
+}
